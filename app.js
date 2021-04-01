2428c93a8bf4d9507dd86d503d483f7146628614
Xiande Qiu <Xiandeqiu@gmail.com>
2021-04-01 13:18:59 +0200

    done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // API Url
 const url = 'http://ec2-35-181-5-201.eu-west-3.compute.amazonaws.com:8080/list-products/'
 const idTeam = 'antelopes' // CHANGEME
+var idProduct = 0;
 
 retreiveAllProductsFromServer();
 
@@ -12,7 +13,7 @@ class Product {
     this.title = title;
     this.price = price;
     this.year = year;
-    
+
   }
 }
 
@@ -42,10 +43,9 @@ class UI {
   static deleteProduct(event) {
     console.log("event", event)
     event.target.closest("div.card.text-center.mb-4").remove();
-    console.log("dw"+event.target.id)
+    console.log("dw" + event.target.id)
     deleteIdProduct(event.target.id);
     UI.showMessage("Product removed successfully", "danger");
-    
   }
 
   static showMessage(message, cssClass) {
@@ -70,34 +70,19 @@ class UI {
 
 
 document.getElementById("product-form").addEventListener("submit", e => {
-    const name = document.getElementById("product-name").value
-    price = document.getElementById("product-price").value
-    var age = document.getElementById("product-year").value
-
-    const product = new Product(name, price, age);
-
-    postData(product)
-
-    UI.addProduct(product);
-    UI.resetForm();
-    UI.showMessage("Product added successfully", "success");
-  
-    e.preventDefault();
-  });
-
-
-document.getElementById("product-form").addEventListener("submit",  e => {
-  /*const name = document.getElementById("product-name").value
+  const name = document.getElementById("product-name").value
   price = document.getElementById("product-price").value
-  year = document.getElementById("product-year").value
+  var age = document.getElementById("product-year").value
 
-  //Save product
-  const product = new Product(name, price, year);
+  postData({
+    title: name,
+    price: price,
+    year: age
+  })
 
-  UI.addProduct(product);
   UI.resetForm();
   UI.showMessage("Product added successfully", "success");
-*/
+
   e.preventDefault();
 });
 
@@ -107,37 +92,36 @@ function postData(opts) {
     method: "POST",
     body: JSON.stringify(opts),
     headers: {
-        "Content-type": "application/json; charset=UTF-8"
+      "Content-type": "application/json; charset=UTF-8"
     }
-})
-.then(response => response.json())
-.then(json => console.log(json));
+  })
+    .then(response => response.json())
+    .then(function(json){
+      console.log(json)
+        opts.id = json.id
+        UI.addProduct(opts);
+    } );
 }
-
 
 function retreiveAllProductsFromServer() {
-    fetch(url+idTeam)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    for(var i = 0; i< myJson.length; i++){
-      if(myJson[i].year <= 2021){
-       UI.addProduct(myJson[i])
+  fetch(url + idTeam)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (myJson) {
+      for (var i = 0; i < myJson.length; i++) {
+        if (myJson[i].year <= 2021) {
+          UI.addProduct(myJson[i])
+        }
       }
-    }
-  });
-  
+    });
 }
 
 
-function deleteIdProduct(id){
-  console.log(id);
+function deleteIdProduct(id) {
+  
   const urlDelete = `http://ec2-35-181-5-201.eu-west-3.compute.amazonaws.com:8080/delete-product/antelopes/${id}`;
- console.log(urlDelete);
   fetch(urlDelete, {
-      method: 'DELETE'
+    method: 'GET'
   })
-  .then(res => res.json())
-  .then(res => console.log(res));
 }
